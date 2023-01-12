059391685afc163b66caa52e3e152b41106c31a6
Rogelio <rogelio@getauru.com>
2023-01-12 09:30:35 -0500

    first

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+
+import { FunctionComponent } from 'react';
+
+
+
+const HomePage: FunctionComponent = () => {
+  return (
+    <div>
+     <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+
+
+export default HomePage;
